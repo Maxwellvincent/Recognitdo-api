7ffa6a349a232069f17985457a465ddbbccfc3e3
Louis Maxwell <louismaxwell@knights.ucf.edu>
2020-11-02 18:39:24 -0500

    updated routes to get 1 user

--- server.js
+++ server.js
@@ -41,8 +41,10 @@ app.get('/', (req,res) => {
 
 
 app.post('/signin', (req,res) => {
+    console.log(req.body)
     if(req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-        res.json('success');
+        // res.json('success');
+        res.json(database.users[0]);
     }else {
         res.status(400).json('error logging in');
     }
@@ -59,7 +61,6 @@ app.post('/register', (req, res) => {
             id: '123',
             name: name,
             email: email,
-            password: password,
             entries: 0,
             joined: new Date()
     });
