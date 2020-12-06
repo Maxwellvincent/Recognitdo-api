290c7284d47dc215c62d30ea43e3d9d684fc98c4
Louis Maxwell <louismaxwell@knights.ucf.edu>
2020-12-06 13:50:00 -0500

    needed to add jwtkey to heroku

--- knexfile.js
+++ knexfile.js
@@ -4,9 +4,9 @@ module.exports = {
 
   development: {
     client: 'pg',
-    // connection: "postgres://localhost/smart-brain"
+    connection: "postgres://localhost/smart-brain"
     // connection: "postgres://localhost/smart_brain" // for Mac 
-    connection: process.env.DATABASE_URL
+    // connection: process.env.DATABASE_URL
   },
 
   test: {
